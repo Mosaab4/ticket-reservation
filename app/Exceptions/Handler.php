5569794386289239fe2742138e373431b05e4ca1
Mosaab <musab.ehab@gmail.com>
2023-02-04 15:33:42 +0200

    modify handler and add ResponsableTrait

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,49 +2,76 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Traits\ResponsableTrait;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
+    use ResponsableTrait;
+
     protected $levels = [
         //
     ];
 
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
-     */
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
     public function register()
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if (!$request->expectsJson()) {
+            return parent::render($request, $e);
+        }
+
+        if ($e instanceof ValidationException) {
+            return $this->setStatusCode(statusCode: 422)
+                ->setStatus(status: false)
+                ->respondWithError(
+                    message: $e->validator->errors()->first()
+                );
+        }
+
+        if ($e instanceof MethodNotAllowedException){
+            return $this->setStatusCode(statusCode: 405)
+                ->setStatus(status: false)
+                ->respondWithError(
+                    message: $e->getMessage()
+                );
+        }
+
+        if ($e instanceof NotFoundHttpException || $e instanceof ModelNotFoundException) {
+            return $this->setStatus(status: false)->respondNotFound();
+        }
+
+        if ($e instanceof AuthenticationException || $e instanceof UnauthorizedException) {
+            return $this->setStatus(status: false)
+                ->respondUnauthorized(
+                    message: $e->getMessage()
+                );
+        }
+
+        return $this->setStatusCode(statusCode: 500)
+            ->setStatus(status: false)
+            ->respondWithError(message: $e->getMessage());
     }
 }
---
+++ app/Traits/ResponsableTrait.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response as IlluminateResponse;
+
+trait ResponsableTrait
+{
+    protected int $statusCode = 200;
+    protected bool $status = true;
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    public function setStatusCode($statusCode): static
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    public function getStatus(): bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status): static
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function respond($data, $message = 'success'): JsonResponse
+    {
+        $code = $this->getStatusCode();
+
+        return response()->json([
+            'status_code'   =>      $this->statusCode,
+            'status'        =>      $this->status,
+            'message'       =>      $message,
+            'data'          =>      $data,
+        ], $code);
+    }
+
+    public function respondWithError($message): JsonResponse
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $message,
+            ],
+        ], 'Request failed');
+    }
+
+    public function respondNotFound($message = 'Not Found'): JsonResponse
+    {
+        return $this
+            ->setStatus(false)
+            ->setStatusCode(IlluminateResponse::HTTP_NOT_FOUND)
+            ->respondWithError($message);
+    }
+
+    protected function respondUnauthorized($message = 'Unauthorized'): JsonResponse
+    {
+        return $this
+            ->setStatus(false)
+            ->setStatusCode(IlluminateResponse::HTTP_UNAUTHORIZED)
+            ->respondWithError($message);
+    }
+
+    protected function respondForbidden($message = 'Forbidden'): JsonResponse
+    {
+        return $this
+            ->setStatus(false)
+            ->setStatusCode(IlluminateResponse::HTTP_FORBIDDEN)
+            ->respondWithError($message);
+    }
+}

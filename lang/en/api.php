680f643a049e5045588d22a8aa290081d823951b
Mosaab <musab.ehab@gmail.com>
2023-02-04 15:43:21 +0200

    add api messages file

---
+++ lang/en/api.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'seat_is_not_available'  => 'This Seat: :seat is not available please try another one',
+    'session_expired'        => 'Your session has expired',
+    'no_remaining_seats'     => "There's no remaining seats in this trip",
+    'no_trip'                => "There's no such trip, please try again",
+    'can_not_create_session' => "Can't Start a new session, please try later",
+];

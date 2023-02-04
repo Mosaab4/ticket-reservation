e526063433e4dfe75ca4f726e9ae24ccfb0ba340
Mosaab <musab.ehab@gmail.com>
2023-02-04 15:35:09 +0200

    add database for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,16 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    protected function beforeRefreshingDatabase()
+    {
+        Config::set('database.default', 'sqlite_testing');
+    }
 }
